8b7cca8551166c4d8c087118e8b166de242a4be1
4kash <ksomani@umass.edu>
2021-06-01 16:38:42 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+	<div class="object-center">
+			<img src="assets/logo.png" alt="logo">
+	</div>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+  	@tailwind components;
+  	@tailwind utilities;
+
+
+	@layer utilities {
+  		.ml-55 {
+    		margin-left: 55%;
+		}
+	}
+</style>
