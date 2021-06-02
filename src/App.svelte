1ccd68237c4134b762b417494a6be176b7249fcc
4kash <ksomani@umass.edu>
2021-06-02 11:50:25 +0530

    global css changes

--- src/App.svelte
+++ src/App.svelte
@@ -2,20 +2,31 @@
 </script>
 
 <main>
-	<div class="flex flex-col justify-center items-center">
-			<img src="assets/logo.png" alt="logo">
-	</div>
+  <div class="Title">
+    <img src="assets/logo.png" alt="logo" class="w-16" />
+    <div class="heading">Learners' Academy</div>
+  </div>
 </main>
 
 <style global lang="postcss">
-	@tailwind base;
-  	@tailwind components;
-  	@tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 
-
-	@layer utilities {
-  		.ml-55 {
-    		margin-left: 55%;
-		}
+  @layer utilities {
+    .ml-55 {
+      margin-left: 55%;
+    }
+  }
+	
+  .heading {
+    font-style: normal;
+    font-weight: bold;
+    text-align: center;
+    color: #00923c;
+		@apply text-2xl
+  }
+	.Title{
+		@apply flex flex-col justify-center items-center;
 	}
 </style>
