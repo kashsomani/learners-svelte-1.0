31249b69b9216e070a397f1b9adaef1d03ac966e
Kash Somani <58804548+kashsomani@users.noreply.github.com>
2021-06-11 12:31:12 +0530

    centered sample video on activities page

---
+++ src/components/Activities.svelte
@@ -0,0 +1,13 @@
+<div class="grid grid-cols-1 place-items-center w-full">
+	<div>
+		<iframe
+			width="560"
+			height="315"
+			src="https://www.youtube.com/embed/hlWiI4xVXKY"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+		/>
+	</div>
+</div>
