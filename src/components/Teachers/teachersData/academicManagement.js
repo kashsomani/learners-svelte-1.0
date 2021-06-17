750c5edcf58130798e84de8689ae211fd8c1a7d0
Kash Somani <kash@kashsomani.com>
2021-06-17 20:14:29 +0530

    added photos and organized code

--- src/components/Teachers/teachersData/academicManagement.js
+++ src/components/Teachers/teachersData/academicManagement.js
@@ -1,27 +1,33 @@
+const teachersDirectory = "assets/Teachers"
 export const academicManagementData = [{
-    name: "Shabana",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", src: "assets/Teachers/Amina.png",
+    name: "Shabana Afsar",
+    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", 
+    src: `${teachersDirectory}/Shabana.jpg`,
     email: "info@learnersacademy.education"
 
 },
 {
     name: "Francisca",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", src: "assets/Teachers/Amina.png",
+    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", 
+    src: `${teachersDirectory}/Amina.png`,
     email: "info@learnersacademy.education"
 
 },
 {
     name: "Brunella",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", src: "assets/Teachers/Amina.png",
+    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", 
+    src: `${teachersDirectory}/Amina.png`,
     email: "info@learnersacademy.education"
 },
 {
     name: "Urvashi",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", src: "assets/Teachers/Amina.png",
+    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", 
+    src: `${teachersDirectory}/Amina.png`,
     email: "info@learnersacademy.education"
 },
 {
     name: "Amina",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", src: "assets/Teachers/Amina.png",
+    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.", 
+    src: `${teachersDirectory}/Amina.png`,
     email: "info@learnersacademy.education"
 }];
