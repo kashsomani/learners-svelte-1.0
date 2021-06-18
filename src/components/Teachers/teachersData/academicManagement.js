b793ff10300fe05fe91e7be58589c469dc89a420
Kash Somani <kash@kashsomani.com>
2021-06-18 09:11:47 +0530

    Update academicManagement.js

--- src/components/Teachers/teachersData/academicManagement.js
+++ src/components/Teachers/teachersData/academicManagement.js
@@ -1,34 +1,35 @@
 const teachersDirectory = "./assets/teachers"
 const src = (dir) => `${teachersDirectory}/${dir}`
-export const academicManagementData = [{
-    name: "Shabana Afsar",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
-    src: src("Shabana.jpg"),
-    email: "admin@learnersacademy.education"
+// {
+//     name: "Shabana Afsar",
+//     info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
+//     src: src("Shabana.jpg"),
+//     email: "admin@learnersacademy.education"
 
-},
-{
-    name: "Francisca",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
-    src: src("Amina.png"),
-    email: "admin@learnersacademy.education"
+// },
+export const academicManagementData = [
+    {
+        name: "Francisca",
+        info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
+        src: src("Amina.png"),
+        email: "admin@learnersacademy.education"
 
-},
-{
-    name: "Brunella",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
-    src: src("Amina.png"),
-    email: "admin@learnersacademy.education"
-},
-{
-    name: "Urvashi",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
-    src: src("Amina.png"),
-    email: "admin@learnersacademy.education"
-},
-{
-    name: "Amina",
-    info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
-    src: src("Amina.png"),
-    email: "admin@learnersacademy.education"
-}];
+    },
+    {
+        name: "Brunella",
+        info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
+        src: src("Amina.png"),
+        email: "admin@learnersacademy.education"
+    },
+    {
+        name: "Urvashi",
+        info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
+        src: src("Amina.png"),
+        email: "admin@learnersacademy.education"
+    },
+    {
+        name: "Amina",
+        info: "Magni inventore repellat dignissimos eveniet dolore ex sit illo adipisci accusamus quos.",
+        src: src("Amina.png"),
+        email: "admin@learnersacademy.education"
+    }];
