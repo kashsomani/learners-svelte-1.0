ec1de9d16c346128368eef80c4b8869aceff3f67
Kash Somani <kash@kashsomani.com>
2021-07-01 14:40:09 +0530

    Update officeStaff.js

--- src/components/Teachers/teachersData/officeStaff.js
+++ src/components/Teachers/teachersData/officeStaff.js
@@ -1,18 +1,17 @@
-const teachersDirectory = "./assets/teachers/office-staff"
-const src = (dir) => `${teachersDirectory}/${dir}`
-const email = "support@learnersacademy.education"
+const teachersDirectory = "./assets/teachers/office-staff";
+const src = (dir) => `${teachersDirectory}/${dir}`;
+const email = "support@learnersacademy.education";
 export const officeStaffData = [
-    {
-        name: "Pooja Ahuja",
-        info: "Mrs. Ahuja is our office manager",
-        src: src("PoojaAhuja.png"),
-        email: email
-
-    },
-    {
-        name: "Rutuja Padwal",
-        info: "Ms. Padwal is our office administrator",
-        src: src("RutujaPadwal.png"),
-        email: email
-
-    }];
+	{
+		name: "Pooja Ahuja",
+		info: "Mrs. Ahuja is our office manager",
+		src: src("PoojaAhuja.png"),
+		email: email,
+	},
+	{
+		name: "Pooja Padwal",
+		info: "Ms. Padwal is our office administrator",
+		src: src("RutujaPadwal.png"),
+		email: email,
+	},
+];
