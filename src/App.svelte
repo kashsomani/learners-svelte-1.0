a7503449fa08fe0e47699f5e4fd9f221f2620991
Kash Somani <kash@kashsomani.com>
2021-07-19 16:00:39 +0530

    added 3d classes

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 <main>
 	<SideNav />
-	<div class="Title">
+	<div class="Title embossed">
 		<img src="assets/logo.png" alt="logo" class="w-16" />
 		<div class="heading"><h1>Learners' Academy</h1></div>
 		<div id="motto">Life is Learning</div>
@@ -22,7 +22,24 @@
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-
+	.embossed-depressed{
+		border-radius: 50px;
+background: linear-gradient(145deg, #9ed1a5, #bcf8c4);
+box-shadow:  20px 20px 25px #96c59c,
+             -20px -20px 25px #caffd2;
+	}
+	.embossed{
+		border-radius: 50px;
+background: #b0e8b7;
+box-shadow:  10px 10px 25px #96c59c,
+             -10px -10px 25px #caffd2;
+	}
+	.depressed{
+		border-radius: 76px;
+background: #b0e8b7;
+box-shadow: inset 26px 26px 64px #9dcea3,
+            inset -26px -26px 64px #c3ffcb;
+	}
 	main {
 		flex: 1 1 auto;
 	}
@@ -42,6 +59,6 @@
 		top: 0%;
 	}
 	.Title {
-		@apply flex flex-col justify-center items-center p-4;
+		@apply flex flex-col justify-center items-center p-4 m-20;
 	}
 </style>
